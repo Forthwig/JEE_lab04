01d218aa6d5109930bc9d63211561a0e652a99cc
MatD3mons <matthieu.desmarescaux@isen.yncrea.fr>
2021-02-01 15:41:17 +0100

    WebConfig

--- lab04-web/src/main/java/junia/lab04/web/config/WebConfig.java
+++ lab04-web/src/main/java/junia/lab04/web/config/WebConfig.java
@@ -2,6 +2,7 @@ package junia.lab04.web.config;
 
 import com.shieldsolutions.velocity.view.VelocityConfigurer;
 import com.shieldsolutions.velocity.view.VelocityViewResolver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,9 +14,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan(basePackages = "junia.lab04.web.controller")
 public class WebConfig implements WebMvcConfigurer {
 
+    @Bean
     private VelocityConfigurer velocityConfigurer;
 
+    @Bean
     private VelocityViewResolver velocityViewResolver;
+
+
 
     public WebConfig() {
         velocityConfigurer.setResourceLoaderPath("/WEB-INF/velocity");
