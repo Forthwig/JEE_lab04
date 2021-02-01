aae08500e67c9245c798b473d5c869e542af9a8a
Thomas <thomas.dubois@isen.yncrea.fr>
2021-02-01 16:07:48 +0100

    -CompanyController-

--- lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
+++ lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
@@ -1,6 +1,10 @@
 package junia.lab04.web.controller;
 import junia.lab04.core.service.CompanyService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import javax.inject.Inject;
 
 @Controller
@@ -8,6 +12,12 @@ public class CompanyController {
 
     @Inject
     private CompanyService companyService;
+
+    @RequestMapping(path = "/list", method = RequestMethod.GET)
+    public String getListOfCompanies(ModelMap modelMap){
+        modelMap.put("companies",this.companyService.findAllWithProjects());
+        return  "companiesList";
+    }
 
     
 
