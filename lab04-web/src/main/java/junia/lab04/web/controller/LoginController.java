dc0824f091e14b8d8ce878be5f47d73187f8765f
MatD3mons <matthieu.desmarescaux@isen.yncrea.fr>
2021-02-19 14:44:23 +0100

    lancement de Tomcat

--- lab04-web/src/main/java/junia/lab04/web/controller/LoginController.java
+++ lab04-web/src/main/java/junia/lab04/web/controller/LoginController.java
@@ -1,13 +1,9 @@
 package junia.lab04.web.controller;
 
-import junia.lab04.core.entity.Ingenieur;
-import junia.lab04.core.service.IngenieurService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
